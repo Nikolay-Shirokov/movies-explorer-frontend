562517a4e8b2c77d0dd359cff371e107f75cf471
Nick Shirokov <nick_4312@mail.ru>
2022-03-07 11:27:52 +0300

    feat: Свертана шапка

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,15 @@
+import './Header.css';
+import { Link, useLocation } from "react-router-dom";
+
 function Header() {
   return (
-    <>
-    </>
+    <header className="header">
+      <Link to='/' className="header__logo" aria-label="Перейти на главную"></Link>
+      <nav className="header__nav-links">
+        <Link to='/signin' className="header__link" aria-label="Регистрация">Регистрация</Link>
+        <Link to='/signup' className="header__link header__link_type_button" aria-label="Войти под существующим пользователем">Войти</Link>
+      </nav>
+    </header>
   );
 }
 
