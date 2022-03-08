4032d35de396503d9fe323540cd751d112fded61
Nikolay Shirokov <nick_4312@mail.ru>
2022-03-08 13:46:57 +0300

    feat: Сверстан блок Potrfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,8 +2,29 @@ import './Portfolio.css';
 
 function Portfolio() {
   return (
-    <>
-    </>
+    <section className="portfolio">
+      <h2 className="portfolio__title">Портфолио</h2>
+      <ul className="portfolio__list">
+        <li className="portfolio__item">
+          <a href="https://nikolay-shirokov.github.io/how-to-learn/" className="portfolio__link" target="_blank" rel="noreferrer">
+            <p className="portfolio__link-text">Статичный сайт</p>
+            <snap className="portfolio__link-arrow"></snap>
+          </a>
+        </li>
+        <li className="portfolio__item">
+          <a href="https://nikolay-shirokov.github.io/russian-travel/" className="portfolio__link" target="_blank" rel="noreferrer">
+            <p className="portfolio__link-text">Адаптивный сайт</p>
+            <snap className="portfolio__link-arrow"></snap>
+          </a>
+        </li>
+        <li className="portfolio__item">
+          <a href="https://nikolay-shirokov.github.io/react-mesto-auth/" className="portfolio__link" target="_blank" rel="noreferrer">
+            <p className="portfolio__link-text">Одностраничное приложение</p>
+            <snap className="portfolio__link-arrow"></snap>
+          </a>
+        </li>
+      </ul>
+    </section>
   );
 }
 
