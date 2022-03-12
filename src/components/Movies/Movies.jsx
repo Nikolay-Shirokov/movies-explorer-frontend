f4277d8fb30c4f1fbf2402e414c09599c3073bae
Nikolay Shirokov <nick_4312@mail.ru>
2022-03-12 07:06:41 +0300

    feat: Добавлен прелоудер

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,10 +1,12 @@
 import SearchForm from '../SearchForm/SearchForm';
+import Preloader from '../Preloader/Preloader';
 import './Movies.css';
 
 function Movies() {
   return (
     <main className="movies">
       <SearchForm/>
+      <Preloader/>
     </main>
   );
 }
