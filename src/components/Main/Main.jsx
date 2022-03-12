06fd9c37c3a19ab0f793f3e858ff9b9a68c9ffd6
Nikolay Shirokov <nick_4312@mail.ru>
2022-03-12 06:24:32 +0300

    feat: Прижатие подвала к низу экрана

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,10 +4,11 @@ import NavTab from "../NavTab/NavTab";
 import Portfolio from "../Portfolio/Portfolio";
 import Promo from "../Promo/Promo";
 import Techs from "../Techs/Techs";
+import './Main.css';
 
 function Main() {
   return (
-    <main>
+    <main className="main">
       <Promo></Promo>
       <NavTab></NavTab>
       <AboutProject></AboutProject>
