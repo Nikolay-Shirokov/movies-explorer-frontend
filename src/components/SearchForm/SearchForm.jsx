f75c1a9f5545c2f46911b0e395f121fe4a497661
Nikolay Shirokov <nick_4312@mail.ru>
2022-03-21 16:58:22 +0300

    fix: Убрано отображение кнопки еще для страницы сохраненных файлов

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -5,10 +5,10 @@ function SearchForm() {
   return (
     <section className="search">
       <form className="search__form">
-        <input className="search__input" placeholder="Фильм" />
+        <input className="search__input" placeholder="Фильм" required />
         <button type="submit" className="search__button active-element"></button>
       </form>
-      <FilterCheckbox/>
+      <FilterCheckbox />
     </section>
   );
 }
