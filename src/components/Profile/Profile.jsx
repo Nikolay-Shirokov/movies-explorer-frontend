8b66ab75c14aa52a8bbef8bca84ab150e2e528c5
Nick Shirokov <nick_4312@mail.ru>
2022-04-03 18:42:08 +0300

    feat: На странице профиля подготовлена возможность редактирования

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,18 +3,20 @@ import './Profile.css';
 function Profile(props) {
   return (
     <main className="profile">
-      <h2 className="profile__title">Привет, <span className="profile__name">Виталий</span>!</h2>
-      <ul className="profile__fields">
-        <li className="profile__field">
-          <p className="profile__label">Имя</p>
-          <p className="profile__value">Виталий</p>
-        </li>
-        <li className="profile__field">
-          <p className="profile__label">E-mail</p>
-          <p className="profile__value">pochta@yandex.ru</p>
-        </li>
-      </ul>
-      <button className="profile__edit-button active-element">Редактировать</button>
+      <form className="profile__form">
+        <h2 className="profile__title">Привет, <span className="profile__name">Виталий</span>!</h2>
+        <ul className="profile__fields">
+          <li className="profile__field">
+            <label className="profile__label">Имя</label>
+            <input className="profile__value" value={"Виталий"}/>
+          </li>
+          <li className="profile__field">
+            <label className="profile__label">E-mail</label>
+            <input className="profile__value" value={"pochta@yandex.ru"}/>
+          </li>
+        </ul>
+        <button className="profile__edit-button active-element" type="submit">Редактировать</button>
+      </form>
       <button className="profile__signout-button active-element">Выйти из аккаунта</button>
     </main>
   );
