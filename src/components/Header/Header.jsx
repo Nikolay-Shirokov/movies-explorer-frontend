88629eb37646aca1378ff3433ac718fb6978225c
Nick Shirokov <nick_4312@mail.ru>
2022-04-09 14:05:36 +0300

    fix: Исправлены ссылки

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,8 +19,8 @@ function Header(props) {
       <Link to='/' className="header__logo active-element" aria-label="Перейти на главную"></Link>
       {!isLogged ?
         <nav className="header__nav-links">
-          <Link to='/signin' className="header__link active-element" aria-label="Регистрация">Регистрация</Link>
-          <Link to='/signup' className="header__link header__link_type_button active-element" aria-label="Войти под существующим пользователем">Войти</Link>
+          <Link to='/signup' className="header__link active-element" aria-label="Регистрация">Регистрация</Link>
+          <Link to='/signin' className="header__link header__link_type_button active-element" aria-label="Войти под существующим пользователем">Войти</Link>
         </nav>
         :
         <>
